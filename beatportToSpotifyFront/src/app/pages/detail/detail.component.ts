5b8488b54e8b614b41ca49812938450bd0a255b3
Andres Suarique <andresuarique@gmail.com>
2024-01-24 19:54:46 -0500

    delete comments

--- beatportToSpotifyFront/src/app/pages/detail/detail.component.ts
+++ beatportToSpotifyFront/src/app/pages/detail/detail.component.ts
@@ -50,8 +50,6 @@ export class DetailComponent implements OnInit {
         this.image=data.playlist.images[0].url
         this.playlistUrl=data.playlist.external_urls.spotify;
         this.tracks = data.tracks.items;
-        //this.tracks.sort(() => Math.random() - 0.5);
-        //this.tracks= this.tracks.slice(0,40);
         this.ready=true;
         this.hasError = false;
 
